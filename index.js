68da01c0ae2ed9ea4ecf06bfd0f9513d927a002a
Sterling <sterlingbarton81@gmail.com>
2023-03-23 10:24:34 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(arr){
+    const result = arr.find(function(arr){
+        return arr.result === 'W'
+    })
+    return !!result ? result.year : undefined
+}
+
+
+
